1118dd2e53960ee36558364f64038b1f707ea4e1
Ahmad Faiz Agustianto <107536877+Mifaki@users.noreply.github.com>
2023-11-20 19:33:23 +0700

    `feat`: Add Community Galert Page

---
+++ script/community.js
@@ -0,0 +1,28 @@
+// community.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const imageGridSection = document.getElementById("imageGridSection");
+    const imageBtn = document.querySelector(".image-btn");
+  
+    const imageFilenames = Array.from({ length: 21 }, (_, index) => `./assets/galery-${index + 1}.jpg`);
+    const imagesToShow = imageFilenames.slice(0, 9);
+  
+    function generateImages(images) {
+      imageGridSection.innerHTML = "";
+      images.forEach((image) => {
+        const imgElement = document.createElement("img");
+        imgElement.src = image;
+        imgElement.alt = "Gallery Image";
+        imageGridSection.appendChild(imgElement);
+      });
+    }
+  
+    imageBtn.addEventListener("click", function () {
+      generateImages(imageFilenames);
+      imageBtn.style.display = "none";
+    });
+
+    console.log(imagesToShow);
+    generateImages(imagesToShow);
+  });
+  
