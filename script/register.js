2993c24fb8294b6d169a3f806dd7f58f7bf1839c
Ahmad Faiz Agustianto <107536877+Mifaki@users.noreply.github.com>
2023-11-24 03:59:20 +0700

    `fix`: Fixing Header Not Detecting userData and Register Form Bug

--- script/register.js
+++ script/register.js
@@ -1,23 +1,21 @@
-// register.js
-
 document.addEventListener("DOMContentLoaded", function () {
-    const signUpForm = document.querySelector(".auth-form");
-  
-    signUpForm.addEventListener("submit", function (event) {
-      event.preventDefault();
-  
-      const fullName = document.getElementById("name").value;
-      const email = document.getElementById("email").value;
-      const password = document.getElementById("password").value;
-  
-      const userData = {
-        fullName,
-        email,
-        password,
-      };
-  
-      localStorage.setItem("userData", JSON.stringify(userData));
-      window.location.href = "./login.html";
-    });
+  const signUpForm = document.getElementById("signUpForm");
+
+  signUpForm.addEventListener("submit", function (event) {
+    console.log("p");
+    event.preventDefault();
+    console.log("p");
+    const fullName = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const userData = {
+      fullName,
+      email,
+      password,
+    };
+
+    localStorage.setItem("userData", JSON.stringify(userData));
+    window.location.href = "./login.html";
   });
-  
+});
